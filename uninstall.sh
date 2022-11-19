f43174617992d9ca660a9205c19d68b8a152c770
Daniel Castelao Suárez <48926694+dcastelaosua@users.noreply.github.com>
2022-11-19 15:34:30 +0100

    Create uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+HOSTNAME=$(grep HOSTNAME .env | awk -F '=' '{printf $2}')
+RP_CONTAINERS=$(docker container ls | grep ${HOSTNAME} | awk '{printf $2"\n" }' | sort | uniq | awk -F ':' '{print $1":"$2" "}')
+if [[ $RP_CONTAINERS == "" ]]; then
+  echo "There are no Rapidpro containers with hostname: ${HOSTNAME}" 
+  exit 1
+fi
+
+echo "Stopping Rapidpro docker containers..."
+if ! docker-compose down; then
+  echo "Failed docker-compose down"
+fi
+
+echo "Removing Rapidpro docker unused images..."
+docker rmi $(docker images -q -f dangling=true) &> /dev/null
+yes | docker system prune &> /dev/null
+
+echo "Removing database volume..."
+rm -rf $(pwd)/data
+
+echo "Removing Nginx locations..."
+rm /etc/nginx/upstream/${HOSTNAME}.conf
+service nginx restart
